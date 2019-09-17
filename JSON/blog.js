6d7773a3c0e57c89115687c4ce429e2633f0edea
christian@baltzer.de <christian@baltzer.de>
2019-09-17 13:10:09 +0200

    added some context

--- JSON/blog.js
+++ JSON/blog.js
@@ -2,11 +2,11 @@ var Save = {
   BlogEntries: [
     {
       H1: 'What is robots.txt and how have you configure it for your site?',
-      Text: 'The Robots.txt is a file, in which the website owner can give instructions to web-crawlers. Web-crawlers are programms that search the web, for example the google search engine. <br>In our case, we told the robot to not enter the "contact.html" page, or the folder "pictures". To use it, you can simply insert the file into your root folder. '
+      Text: 'The Robots Exclusion Protocol is a protocol, in which the website owner can give instructions to web-crawlers. Web-crawlers are programms that search the web, for example the google search engine. For the protocoll, se st up a robots.txt file. the programm will search it and hopefully follow it. <br>In our case, we told the robot to not enter the "contact.html" page, or the folder "pictures". To use it, you can simply insert the file into your root folder.<br> <a href="./robots.txt">To see the file click here.</a> <br>When you implement a file like this, you should always remember, that this is not a real protection. Crawlers can simply be programmed to ignore the order.'
     },
     {
       H1: 'What is humans.txt and how have you configure it for your site?',
-      Text: 'The humans.txt is a file that contains information about the Developers of the site and there used tools and standarts. You can also thank people or organisations. <br>I\'ve inserted my name, how to cantact me and my used editor. <br>Tp use it, you can implement a link in your page, but you can also put it in the root folder.'
+      Text: 'The humans.txt is a file that contains information about the Developers of the site and there used tools and standarts. You can also thank people or organisations. <br>I\'ve inserted my name, how to cantact me and my used editor. <br> <a href="./humans.txt">To see the file click here.</a> <br>Tp use it, you can implement a link in your page, but you can also put it in the root folder.'
     },
     {
       H1: 'What is Open Graph and how do you make use of it?',
